0357827983bdaab57df79e099107c15ddcf1e4b0
tiffanyplus <100295960+tiffanyplus@users.noreply.github.com>
2024-11-07 18:37:24 +0100

    Update localization.lua

--- localization.lua
+++ localization.lua
@@ -358,7 +358,7 @@ TheoryCraft_Locale = {
 		["Greater Heal"] = "Greater Heal",
 		["Devouring Plague"] = "Devouring Plague",
 		["Renew"] = "Renew",
-		["Swift Recovery"] = "swiftrec",
+		["Swift Recovery"] = "Swift Recovery",
 		["Inner fire"] = "Inner Fire",
 		["Starshards"] = "Starshards",
 
